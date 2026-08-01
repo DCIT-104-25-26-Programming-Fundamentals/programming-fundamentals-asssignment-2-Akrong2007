3a2ea3dcd6bcaa49c071d36f5a3f53577cd26423
Eben Akrong <eakrong010@st.ug.edu.gh>
2026-08-01 15:55:47 +0000

    Add statistics functions for assignment 3

--- assignment_03_array_statistics.cpp
+++ assignment_03_array_statistics.cpp
@@ -42,3 +42,35 @@
 #include <iostream>
 using namespace std;
 
+int calculateSum(int arr[], int n) {
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+double calculateAverage(int arr[], int n) {
+    return (double)calculateSum(arr, n) / n;
+}
+
+int findMax(int arr[], int n) {
+    int max = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+int findMin(int arr[], int n) {
+    int min = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+    return min;
+}
+
